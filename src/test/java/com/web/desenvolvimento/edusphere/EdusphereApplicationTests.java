5a8a967a9e52999b327c54a977aacb7f10523477
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2024-04-09 21:34:12 -0300

    chorie: inicia o projeto spring com sua configuração

---
+++ src/test/java/com/web/desenvolvimento/edusphere/EdusphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.desenvolvimento.edusphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
